6d0864479d73bea3c0024e2682f2b5ff1c11c89d
CrocoDilda <cucuprum4k@gmail.com>
2024-07-22 11:36:49 +0300

    rename component StartComponent to StartPage

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import { provide, ref } from 'vue'
-import StartComponent from './pages/start_component/StartComponent.vue'
+import StartPage from './pages/start_page/StartPage.vue'
 
 // Добавляю имя приложения в переменную для того, что бы с лучае чего можно было легко поменять
 const appName = 'Delayed Viewing'
@@ -11,20 +11,12 @@ provide('appName', appName)
   <header class="header">
     <p class="app-name">{{ appName }}</p>
   </header>
-  <StartComponent v-if="false" />
+  <StartPage v-if="false" />
 </template>
 
 <style scoped>
 .header {
   position: absolute;
   padding: 15px 0 0 15px;
-}
-
-.start--wrapper {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  height: 100vh;
-  padding: 0 15vw;
 }
 </style>
