6c0c14f26e1c441d6150b48f2c2fec2fb3a6767b
CrocoDilda <cucuprum4k@gmail.com>
2024-07-24 21:32:02 +0300

    returned the RegistrationItem component validation that was broken in previous commits

--- src/components/form_component/registration/RegistrationItem.vue
+++ src/components/form_component/registration/RegistrationItem.vue
@@ -8,18 +8,94 @@ import MainButton from '../../../components/controls/button/MainButton.vue'
 
 const emit = defineEmits(['changeActiveForm'])
 
+type UserData = {
+  name: string
+  password: string
+  repeatPassword: string
+}
+
+type Errors = {
+  name?: string
+  password?: string
+  repeatPassword?: string
+}
+
+const userData = ref<UserData>({
+  name: '',
+  password: '',
+  repeatPassword: ''
+})
+
+const errors = ref<Errors>({})
 const errorMessage = ref('')
+
+function isValid(name: string, minLength: number, maxLength: number): string | null {
+  if (!name) return `Required field`
+  if (name.length < minLength) return `Password must be at least ${minLength} characters`
+  if (name.length > maxLength) return `Password must be at no more ${maxLength} characters`
+
+  return null
+}
+
+function validate(): boolean {
+  errors.value = {}
+
+  const nameError = isValid(userData.value.name, 2, 25)
+  if (nameError) errors.value.name = nameError
+
+  const passwordError = isValid(userData.value.password, 6, 25)
+  if (passwordError) errors.value.password = passwordError
+
+  const repeatPasswordError =
+    repeatPass() === null ? isValid(userData.value.repeatPassword, 6, 25) : repeatPass()
+  if (repeatPasswordError) errors.value.repeatPassword = repeatPasswordError
+
+  return !Object.keys(errors.value).length
+}
+
+function repeatPass(): null | string {
+  if (userData.value.password !== userData.value.repeatPassword) {
+    return 'Passwords do not match'
+  }
+  return null
+}
+
+function submit() {
+  if (validate()) {
+    console.log('Form is valid', userData.value)
+  } else {
+    console.log('Form is invalid')
+  }
+}
 </script>
 
 <template>
   <div class="wrappe">
     <div class="inner">
       <h2 class="title">Registration</h2>
-      <LabelItem labelTitle="Unique name" placeholder="My-wonderful_name" inputType="text" />
-      <LabelItem labelTitle="Create password" placeholder="********" inputType="password" />
-      <LabelItem labelTitle="Repeat password" placeholder="********" inputType="password" />
+      <LabelItem
+        v-model="userData.name"
+        :errors="errors.name"
+        labelTitle="Unique name"
+        placeholder="My-wonderful_name"
+        inputType="text"
+      />
+      <LabelItem
+        v-model="userData.password"
+        :errors="errors.password"
+        labelTitle="Create password"
+        placeholder="********"
+        inputType="password"
+      />
+      <LabelItem
+        v-model="userData.repeatPassword"
+        :errors="errors.repeatPassword"
+        labelTitle="Repeat password"
+        placeholder="********"
+        inputType="password"
+      />
       <div class="innerr">
-        <MainButton type="button" class="button" text="Register" />
+        <MainButton @click="submit" type="button" class="button" text="Register" />
         <MainButton
           @click="() => emit('changeActiveForm', 'authorization')"
           type="button"
