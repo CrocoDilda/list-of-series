944aed3db808b260f0d32bfddd444304b8e2a4fb
CrocoDilda <cucuprum4k@gmail.com>
2024-07-25 22:54:24 +0300

    made GetStart component

--- src/components/get_start/GetStart.vue
+++ src/components/get_start/GetStart.vue
@@ -1,7 +1,39 @@
 <script setup lang="ts">
-console.log('GetStart')
+import MainButton from '../../components/controls/button/MainButton.vue'
 </script>
 
 <template>
-  <h1>GetStart</h1>
+  <div class="wrapper">
+    <h2 class="title">
+      В вашей коллекции пока пусто 😞 <br />
+      Нажмите, чтобы <span class="title_green">добавить</span> свой первый
+      <span class="title_green">фильм</span> или <span class="title_green">сериал</span> 🤩
+    </h2>
+    <MainButton text="Get start" class="button" />
+  </div>
 </template>
+
+<style scoped>
+.wrapper {
+  display: grid;
+  justify-items: center;
+  align-items: center;
+  gap: 30px;
+}
+
+.title {
+  display: block;
+  align-self: end;
+  font-size: 1.5rem;
+  line-height: 1.7;
+  text-align: center;
+}
+
+.title_green {
+  color: var(--color-main-green);
+}
+
+.button {
+  align-self: start;
+}
+</style>
